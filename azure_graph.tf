ae5c557695e98f8be7c1885b3b102467e183eca1
amit9415 <amit9415@gmail.com>
2024-11-07 16:11:09 +0530

    Function created on personal account

--- azure_graph.tf
+++ azure_graph.tf
@@ -1,3 +1,4 @@
+/*
 # Define a data block to fetch the object_id from the currently signed Azure AD account
 data "azuread_client_config" "current" {}
 
@@ -22,7 +23,7 @@ resource "random_uuid" "email_sender_administrator" {}
 resource "azuread_application_app_role" "email_sender_permission" { 
   role_id        = random_uuid.email_sender_administrator.id
   application_id = azuread_application.app.id
-  allowed_member_types = ["User"]
+  allowed_member_types =  ["User", "Application"]
   description          = "Admins can manage roles and perform all task actions"
   display_name         = "Administer"
   value                = "admin"
@@ -37,3 +38,67 @@ output "client_secret" {
   sensitive = true
 }
 
+*/
+resource "azurerm_resource_group" "fun-grp" {
+  name     = "fun-grp-nov6-v3"
+  location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "fun-str-acc" {
+  name                     = "funstracc9415"
+  resource_group_name      = azurerm_resource_group.fun-grp.name
+  location                 = azurerm_resource_group.fun-grp.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "fun-serv-plan" {
+  name                = "funservplan9415"
+  resource_group_name = azurerm_resource_group.fun-grp.name
+  location            = azurerm_resource_group.fun-grp.location
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_function_app" "fun-app" {
+  name                = "funapp123123"
+  resource_group_name = azurerm_resource_group.fun-grp.name
+  location            = azurerm_resource_group.fun-grp.location
+
+  storage_account_name       = azurerm_storage_account.fun-str-acc.name
+  storage_account_access_key = azurerm_storage_account.fun-str-acc.primary_access_key
+  service_plan_id            = azurerm_service_plan.fun-serv-plan.id
+
+  depends_on = [ azurerm_service_plan.fun-serv-plan , azurerm_storage_account.fun-str-acc ]
+
+  https_only                  = true
+/*
+   app_settings = {
+    "ENABLE_ORYX_BUILD"              = "true"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
+    "FUNCTIONS_WORKER_RUNTIME"       = "python"
+    "AzureWebJobsFeatureFlags"       = "EnableWorkerIndexing"
+   "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insight.instrumentation_key
+  }
+*/
+  site_config {
+     application_stack {      
+      python_version = "3.11"
+    }
+  }
+
+}
+
+resource "azurerm_storage_container" "mail-sender-code-container" {
+  name                  = "mail-sender-code"
+  storage_account_name  = azurerm_storage_account.fun-str-acc.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "mail-sender-code" {
+  name                   = "mail_sender_code.zip"
+  storage_account_name   =  azurerm_storage_account.fun-str-acc.name
+  storage_container_name =  azurerm_storage_container.mail-sender-code-container.name
+  type                   = "Block"
+  source                 = "./SendMailFunction/mail_sender_code.zip"
+}
